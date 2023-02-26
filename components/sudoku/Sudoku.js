4ab0316fc78e165773c709dabadd64cf17d4afd7
Maxim <bekrinmks@gmail.com>
2023-02-26 05:50:58 +0300

    full fill field no right

--- components/sudoku/Sudoku.js
+++ components/sudoku/Sudoku.js
@@ -18,26 +18,55 @@ class Sudoku {
     addBaseField() {
         let tds = document.querySelectorAll('td');
         console.log(tds);
-
-        let arr = [1,2,3,4,5,6,7,8,9]
-        for(let i = 0; i <= 8; i++) {
-            let randomNum = TechnicalFunc.random(arr.length - 1);
-            tds[i].firstElementChild.textContent = arr[randomNum];
-            console.log(arr);
-            console.log(arr[randomNum])
-            arr.splice(arr.indexOf(arr[randomNum]), 1);
-        };
-
-        let currentNum = 0;
-        let arrColumnOne = [];
+        let currentNum = 0; // текущий номер заполняемой ячейки
+        let arrColumnOne = []; // цифры, содрежащиеся в столбце
         let arrColumnTwo = [];
         let arrColumnThree = [];
+        let arrColumnFour = [];
+        let arrColumnFive = [];
+        let arrColumnSix = [];
+        let arrColumnSeven = [];
+        let arrColumnEight = [];
+        let arrColumnNine = [];
 
-        for(let i = 1; i < 28; i++) {
-            currentNum++;
-            console.log(currentNum);
-            if(currentNum%3 == 0) currentNum += 6;
-        }
+        let arr = [1,2,3,4,5,6,7,8,9];
+        // for(let i = 0; i <= 8; i++) {
+        //     let randomNum = TechnicalFunc.random(arr.length - 1);
+        //     tds[i].firstElementChild.textContent = arr[randomNum];
+        //     console.log(arr);
+        //     console.log(arr[randomNum])
+        //     arr.splice(arr.indexOf(arr[randomNum]), 1);
+        // };
+
+        // for(let i = 1; i < 28; i++) {
+        //     currentNum++;
+        //     let randomNum = TechnicalFunc.random(arr.length - 1);
+        //     tds[currentNum - 1].firstElementChild.textContent = arr[randomNum];
+        //     arr.splice(arr.indexOf(arr[randomNum]), 1);
+        //     if(currentNum%3 == 0) currentNum += 6;
+        //     if(arr.length == 0) arr = [1,2,3,4,5,6,7,8,9];
+        // }
+
+        function fillField (currentNumFunc) {
+            let currentNum = currentNumFunc;
+            for(let i = 1; i < 28; i++) {
+                currentNum++;
+                let randomNum = TechnicalFunc.random(arr.length - 1);
+                tds[currentNum - 1].firstElementChild.textContent = arr[randomNum];
+                arr.splice(arr.indexOf(arr[randomNum]), 1);
+                if(currentNum%3 == 0) currentNum += 6;
+                if(arr.length == 0) arr = [1,2,3,4,5,6,7,8,9];
+            };
+        };
+        fillField(0);
+        fillField(3);
+        fillField(6);
+        // let currentNum = 0;
+        // for(let i = 1; i < 28; i++) {
+        //     currentNum++;
+        //     console.log(currentNum);
+        //     if(currentNum%3 == 0) currentNum += 6;
+        // }
     };
 };
 
